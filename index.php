fa4dafbb117ed8bb49fd730b85fc5366ff9231ef
Baptiste Leduc <baptiste.leduc@gmail.com>
2021-01-14 11:36:49 +0100

    async

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Component\HttpClient\CurlHttpClient;
 
+$i = 0;
+$count = intval($argv[1] ?? 500);
+$client = new CurlHttpClient();
+$responses = [];
+do {
+    $responses[] = $client->request('GET', 'https://httpbin.org/get', ['headers' => ['Accept' => 'application/json']]);
+    ++$i;
+} while ($i < $count);
+
+\sleep(5);
+
+foreach ($responses as $response) {
+    $response->getStatusCode(); // wait for all responses
+}
